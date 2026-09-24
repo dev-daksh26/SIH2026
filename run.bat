b04c39d3e6381dcd2aac05075af07e9d80d492fd
Daksh Saxena <121349450+dev-daksh26@users.noreply.github.com>
2026-09-24 17:23:59 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting BharatVault Server...
-echo (waiting a few seconds for the server to finish starting before opening the browser...)
-start /min cmd /c "timeout /t 3 /nobreak >nul && start http://localhost:8000"
-uvicorn main:app --reload --port 8000
-pause
